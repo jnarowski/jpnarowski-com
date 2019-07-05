652fac05831c10de1651e1a32ae8de933f1ca2e1
jnarowski <jnarowski@gmail.com>
2019-07-05 12:34:39 -0600

    added alt tags

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,9 +11,7 @@ export default {
 <template>
   <b-navbar toggleable="lg" type="light" sticky class="navbar--light">
     <div class="container">
-      <nuxt-link aria-label="Site home link" class="navbar-brand" to="/">
-        JP's Musings
-      </nuxt-link>
+      <nuxt-link aria-label="Site home link" class="navbar-brand" to="/">JP's Musings</nuxt-link>
 
       <b-navbar-toggle target="nav-collapse" />
 
@@ -21,30 +19,30 @@ export default {
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <li class="nav-item">
-            <nuxt-link aria-label="Read more about my books" to="/books" class="nav-link">
-              Books
-            </nuxt-link>
+            <nuxt-link aria-label="Read more about my books" to="/books" class="nav-link">Books</nuxt-link>
           </li>
           <li v-if="false" class="nav-item">
             <nuxt-link
               aria-label="Read about my curiosities"
               to="/curiosities"
               class="nav-link"
-            >
-              Curiosities
-            </nuxt-link>
+            >Curiosities</nuxt-link>
           </li>
           <li class="nav-item">
-            <nuxt-link to="/about-me" aria-label="About me" class="nav-link">
-              About Me
-            </nuxt-link>
+            <nuxt-link to="/about-me" aria-label="About me" class="nav-link">About Me</nuxt-link>
           </li>
           <b-nav-form ref="searchForm" action="/search">
             <div class="search-form">
               <div class="search-icon" @click="submitSearch">
                 <fa class="search" :icon="['fas', 'search']"></fa>
               </div>
-              <b-form-input name="term" size="sm" class="mr-sm-2 search-input" placeholder="Search"></b-form-input>
+              <b-form-input
+                aria-label="Search the site"
+                name="term"
+                size="sm"
+                class="mr-sm-2 search-input"
+                placeholder="Search"
+              ></b-form-input>
             </div>
           </b-nav-form>
         </b-navbar-nav>
