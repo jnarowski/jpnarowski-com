4e97de1cc3730a81be055a61748e1b84a66c4bfa
jnarowski <jnarowski@gmail.com>
2019-07-05 17:01:29 -0600

    a

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,9 @@ export default {
       <div class="mainheading">
         <p
           class="lead"
-        >My thoughts on business, travel, and all the other tidbits life has to offer.</p>
+        >
+          My thoughts on business, travel, and all the other tidbits life has to offer.
+        </p>
       </div>
       <section class="featured-posts">
         <div class="section-title">
@@ -53,7 +55,9 @@ export default {
           <post-card v-for="post in results" :key="post.uid" v-bind="post"></post-card>
         </div>
         <div class="text-center">
-          <nuxt-link class="btn btn-secondary mb-3" to="/posts">See all posts</nuxt-link>
+          <nuxt-link class="btn btn-secondary mb-3" to="/posts">
+            See all posts
+          </nuxt-link>
         </div>
       </section>
       <section class="featured-posts">
